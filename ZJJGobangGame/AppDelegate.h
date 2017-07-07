4ea80d1f94bbac1b691e00dfc0d249fe75ebd501
张锦江 <zhangjinjiang@zhangjinjiangdeMacBook-Pro.local>
2017-07-07 14:18:54 +0800

    Initial Commit

---
+++ ZJJGobangGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZJJGobangGame
+//
+//  Created by 张锦江 on 2017/7/7.
+//  Copyright © 2017年 ZJJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZJJGobangGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZJJGobangGame
+//
+//  Created by 张锦江 on 2017/7/7.
+//  Copyright © 2017年 ZJJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
